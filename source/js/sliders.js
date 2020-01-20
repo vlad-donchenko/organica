9a7f28f2be49cbc4237e0a5bb2c7e3d1440a442a
vlad-donchenko <2pi14b.donchenko@gmail.com>
2020-01-20 15:35:12 +0200

    Добавляет блок отзывов

--- source/js/sliders.js
+++ source/js/sliders.js
@@ -2,6 +2,7 @@
 
 (function () {
   var workSlider = $('.work-slider__slider');
+  var reviews = $('.reviews__slider');
 
   if (workSlider) {
     workSlider.slick({
@@ -9,7 +10,37 @@
       arrows: false,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
+      slidesToShow: 1
+    });
+  }
+
+  if (reviews) {
+    reviews.slick({
+      dots: false,
+      arrows: true,
+      infinite: false,
+      speed: 500,
+      prevArrow: $('.reviews__arrow--prev'),
+      nextArrow: $('.reviews__arrow--next'),
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      responsive: [
+        {
+          breakpoint: 1170,
+          settings: {
+            infinite: true
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            infinite: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            adaptiveHeight: true
+          }
+        }
+      ]
     });
   }
 })();
